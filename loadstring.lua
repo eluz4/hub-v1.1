7b9b05a1ade3981ee69f24f16b7b3670af8d2f1f
eluza <hoggin222@Outlook.com>
2025-01-10 15:31:37 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,6 +1,8 @@
 --[[
     currently only "Legends of Speed", and "Knockout Simulator" support.
     will add more games eventually
+
+    also note that pet duping costs gems (bunny = 5m) (samurai = 100m)
 ]]
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/loader.lua'))()
