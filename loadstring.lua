47f912c783c9eda4bffd67f3f315708ecc069c00
eluza <hoggin222@Outlook.com>
2025-01-10 05:25:46 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,6 @@
+--[[
+    currently only "Legends of Speed", and "Knockout Simulator" support.
+    will add more games eventually
+]]
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/loader.lua'))()
