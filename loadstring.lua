b36b924c4206ad2e7c4b29f4a93e5f2eb53d4106
eluza <hoggin222@Outlook.com>
2025-03-02 04:30:36 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,9 +1,6 @@
 --[[
-    Has Support for "Legends of Speed", "Pull a Sword", and Knockout Simulator.
-
-    also note that pet duping in legends of speed costs gems (bunny = 5m) (samurai = 100m)
-
-    I only post scripts here for "bad" games, or games that dont take long to make something for.
+    Has Support for "Legends of Speed", "Pull a Sword", "Knockout Simulator", and "Realistic Basketball".
+    pet duping on legends of speed costs gems
+    Realistic Basketball is a new script of mine so im slowly adding stuff i see fit be patient
 ]]
-
 loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/loader.lua'))()
