eb0d6d8cf34d97f31d1af3b13a88782cca7fbd40
eluza <hoggin222@Outlook.com>
2025-03-02 04:57:30 -0600

    Update loader.lua

--- main/loader.lua
+++ main/loader.lua
@@ -1,5 +1,5 @@
 if game:IsLoaded() then
-    game.StarterGui:SetCore('SendNotification', {
+    gui:SetCore('SendNotification', {
     Title = 'eluza hub | v1.1',
     Text = 'Checking For Game..',
     Duration = 3,
@@ -8,21 +8,43 @@ else
     game.Loaded:Wait()
 end
 
+local gui = game.StarterGui
+
 if game.PlaceId == 3101667897 or game.PlaceId == 3276265788 or game.PlaceId == 3232996272 then 
     loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/los.lua'))()
 
+        gui:SetCore('SendNotification', {
+        Title = 'eluza hub | v1.1',
+        Text = 'Game Found, Loading "Legends of Speed"!',
+        Duration = 3,
+
 elseif game.PlaceId == 13667319624 then 
     loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/kos.lua'))()
+            
+        gui:SetCore('SendNotification', {
+        Title = 'eluza hub | v1.1',
+        Text = 'Game Found, Loading "Knockout Simulator"!',
+        Duration = 3,
 
 elseif game.PlaceId == 13827198708 then 
     loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/pullasword.lua'))()
 
+        gui:SetCore('SendNotification', {
+        Title = 'eluza hub | v1.1',
+        Text = 'Game Found, Loading "Pull a Sword"!',
+        Duration = 3,
+
 elseif game.PlaceId == 17725704589 or game.PlaceId == 18281472820 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/eluz4/hub-v1.1/refs/heads/main/main/bb.lua'))()
 
+        gui:SetCore('SendNotification', {
+        Title = 'eluza hub | v1.1',
+        Text = 'Game Found, Loading "Realistic Basketball"!',
+        Duration = 3,
+
 else 
     wait(2)
-    game.StarterGui:SetCore('SendNotification', {
+    guii:SetCore('SendNotification', {
     Title = 'eluza hub | v1.1',
     Text = 'No Games Found..',
     Duration = 3,
